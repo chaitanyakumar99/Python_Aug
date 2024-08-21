e5df5fb46d6692510c2086662941486f7a088627
USER 1 <gchkumar999@gmail.com>
2024-08-21 16:34:27 +0530

    txt

---
+++ conditional-st/two.py
@@ -0,0 +1,6 @@
+num =int(input("Enter number:"))
+
+if num >=100 and num<=999:                      
+    print("Given Number is 3 digit number")
+    else:
+        print("Given Number is not 3 digit number")
