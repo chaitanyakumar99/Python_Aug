e643f699bee37729888d91fd7f62c3ed5483f758
USER 1 <gchkumar999@gmail.com>
2024-09-10 23:19:00 +0530

    txt

---
+++ Assign/assinment1.py
@@ -0,0 +1,11 @@
+emp={
+    'id':101,
+    'name':'Rahul',
+    'sal':45000
+}
+
+user={}
+user['id']=emp['id']
+user['name']=emp['name']
+user['sal']=emp['sal']
+print(user)
---
+++ Assign/assinment2.py
@@ -0,0 +1,8 @@
+emp={
+    'id':101,
+    'name':'Rahul',
+    'sal':45000
+}
+
+def updateEmpData(emp):
+    return dict({'id':emp['id'],'name':emp})
